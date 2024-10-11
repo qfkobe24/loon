76b8717936ed1b88a615ddee6f002697ba9ab5b6
qfkobe24 <qfkobe24@163.com>
2024-10-11 17:14:14 +0800

    Update wdc.js

--- script/wdc.js
+++ script/wdc.js
@@ -1,1 +1,21 @@
+/*
+[rewrite_local]
+#修改回源信息
+^https:\/\/i\.at\.qq\.com\/pay\/memberinfo url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/wxts/wxts.js
 
+[mitm]
+hostname = i.at.qq.com,
+
+*/
+
+let obj = JSON.parse($response.body);
+console.log(obj);
+obj = {
+  ...obj,
+  data: {
+    ...obj.data,
+    nickname: "哈哈哈"
+  }
+ };
+
+$done({body: JSON.stringify(obj)});
